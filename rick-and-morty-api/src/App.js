b26e205a3afcd35f95926262ba11cfab298fc06d
Greg <gbrewton1980@gmail.com>
2021-09-24 15:21:32 -0600

    Connects app to API with custom hook

--- rick-and-morty-api/src/App.js
+++ rick-and-morty-api/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useAxios } from "./hooks";
+import { BASE_URL } from "./constants/index";
 
 function App() {
+  const [data, setData] = useState();
+
+  const { response, loading, error } = useAxios({
+    method: "get",
+    url: BASE_URL.characters,
+  });
+
+  useEffect(() => {
+    if (response !== null) {
+      setData(response);
+    }
+  }, [response]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>App Component</h1>
+      {loading ? (
+        <p>...loading</p>
+      ) : (
+        <div>
+          {error && (
+            <div>
+              <p>{error.message}</p>
+            </div>
+          )}
+          <div>{data && <p>{data.info.count} results</p>}</div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ rick-and-morty-api/src/constants/index.js
@@ -0,0 +1,7 @@
+const base = "https://rickandmortyapi.com/api";
+
+export const BASE_URL = {
+  characters: `${base}/character`,
+  episodes: `${base}/episode`,
+  locations: `${base}/location`,
+};
